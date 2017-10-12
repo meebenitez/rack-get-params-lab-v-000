75777e70adec1a88c3943e265878ee7b3ace243e
Mary Benitez <meebenitez@gmail.com>
2017-10-12 04:16:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,6 +23,7 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/add/)
     else
       resp.write "Path Not Found"
     end
