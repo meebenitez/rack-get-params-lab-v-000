0efbb05449f525b8f01674b31018588669c8d212
Mary Benitez <meebenitez@gmail.com>
2017-10-12 04:13:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      if @@cart = nil
+      if @@cart == nil
         resp.write "Your cart is empty\n"
       else
         @@items.each do |item|
